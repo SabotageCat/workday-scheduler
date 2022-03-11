954c62af1c73d7e494cb8326cfd396b5fdca164c
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-03-11 15:01:42 -0600

    time blocks added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,29 @@
 // variables
 var todaysDate =  moment().format("dddd, MMMM Do YYYY");
-var currentTime = moment().hour();
+var currentHour = moment().hour();
+var schedulerHour = $("textarea");
+
 //get todays date and display
 $("#currentDay").append(todaysDate);
 
 // update time color coding
-if (currentTime >= 9) {
-    $("textarea").removeClass("present future past");
-    $("#nine").addClass("present");
-}
-if (currentTime >= 10) {
-    $("textarea").removeClass("present future past");
-    $("#ten").addClass("present");
-}
-if (currentTime >= 11) {
-    $("textarea").removeClass("present future past");
-    $("#eleven").addClass("present");
-}
-if (currentTime >= 12) {
-    $("textarea").removeClass("present future past");
-    $("#twelve").addClass("present");
-}
-if (currentTime >= 13) {
-    $("textarea").removeClass("present future past");
-    $("#one").addClass("present");
-}
-if (currentTime >= 14) {
-    $("textarea").removeClass("present future past");
-    $("#two").addClass("present");
-}
-if (currentTime >= 15) {
-    $("textarea").removeClass("present future past");
-    $("#three").addClass("present");
-}
-if (currentTime >= 16) {
-    $("textarea").removeClass("present future past");
-    $("#four").addClass("present");
-}
-if (currentTime >= 17) {
-    $("textarea").removeClass("present future past");
-    $("#five").addClass("present");
-}
+setInterval(colorHours, 60000);
+    
+var colorHours = function() {
+    for (var i = 0; i < schedulerHour.length; i++) {
+        debugger;
+        if (currentHour == schedulerHour[i].id) {
+            $(schedulerHour[i]).removeClass("past future");
+            $(schedulerHour[i]).addClass("present");
+        } else if (currentHour > schedulerHour[i].id) {
+            $(schedulerHour[i]).removeClass("present future");
+            $(schedulerHour[i]).addClass("past");
+        } else {
+            $(schedulerHour[i]).removeClass("past present");
+            $(schedulerHour[i]).addClass("future");
+        }
+    }
+};
+
+
+colorHours();
